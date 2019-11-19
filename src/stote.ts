000651d462542d1c2e4819ab870c2849529da54b
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-20 00:29:47 +0900

    refactor: store file

--- src/stote.ts
+++ src/stote.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux'
 import { createBrowserHistory } from 'history'
-import { createRootReducer } from '../modules/reducer'
+import { createRootReducer } from './modules/reducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { logger } from 'redux-logger'
 import { routerMiddleware } from 'connected-react-router'
