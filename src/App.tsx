03a6cfd5efc7591b2a03d2caf55d23120024c809
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-20 00:59:48 +0900

    feat: impl dispatch table list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from 'antd'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchTableList } from './modules/table'
+import { ReduxStore } from './modules/reducer'
 
 const App: React.FC = () => {
+  const table = useSelector<ReduxStore, ReduxStore['table']>((state: ReduxStore) => state.table)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchTableList())
+  }, [dispatch])
+
   return (
     <div>
       <Button>テスト</Button>
+      {table.list.map((tableName, index) => (
+        <div key={index}>{tableName}</div>
+      ))}
     </div>
   )
 }
--- src/modules/reducer.ts
+++ src/modules/reducer.ts
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux'
 import { connectRouter, RouterState } from 'connected-react-router'
 import { History } from 'history'
+import { State as TableState, reducer as tableReducer } from './table'
 
 export interface ReduxStore {
   router: RouterState
+  table: TableState
 }
 
 export const createRootReducer = (history: History) =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    table: tableReducer
   })
