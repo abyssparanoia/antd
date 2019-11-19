c736b8d1de6371293200faa75e26ebc13cced8f8
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-20 02:23:17 +0900

    feat: add syntax highlight editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Global } from './components/Global'
+import Editor from 'react-simple-code-editor'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/nightOwl'
 
 const App: React.FC = () => {
-  return <Global />
+  const [sql, setSql] = useState<string>('')
+  const handleChange = (value: string) => setSql(value)
+
+  const highlight = (code: string) => (
+    <Highlight {...defaultProps} theme={theme} code={code} language="sql">
+      {({ tokens, getLineProps, getTokenProps }) => (
+        <>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={i} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </>
+      )}
+    </Highlight>
+  )
+
+  return (
+    <div>
+      <Global />
+      <Editor
+        value={sql}
+        onValueChange={handleChange}
+        highlight={highlight}
+        padding={10}
+        style={{
+          fontFamily: '"Fira code", "Fira Mono", monospace',
+          fontSize: 12
+        }}
+      />
+    </div>
+  )
 }
 
 export default App
