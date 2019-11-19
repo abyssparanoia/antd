8a0d316e3f9b4629b4330c2ef76cde1dad0d1b4c
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-19 23:36:52 +0900

    feat: added provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import 'antd/dist/antd.css'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'connected-react-router'
+import { history, store } from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
+)
