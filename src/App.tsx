ab64497a34181f120ec4ddee39a817bec50d5d05
abyssparanoia <y.sugimoto.paranoia@gmail.com>
2019-11-24 22:54:56 +0900

    feat: added result table

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Global } from './components/Global'
 import { Editor } from './components/organisms/Editor'
+import { ResultTable } from './components/organisms/ResultTable'
 
 const App: React.FC = () => {
   return (
     <div>
       <Global>
         <Editor />
+        <ResultTable />
       </Global>
     </div>
   )
